f300f5bd8b8a7b55a8ace9d8ff8484de6e7583ba
Yousef Saeed <yousefsaeed668@gmail.com>
2024-12-04 22:21:31 +0200

    create signup , login page setup db

---
+++ src/app/(auth)/signup/actions.ts
@@ -0,0 +1,79 @@
+"use server";
+
+import { lucia } from "@/auth";
+import prisma from "@/lib/prisma";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import { hash } from "@node-rs/argon2";
+import { generateIdFromEntropySize } from "lucia";
+import { isRedirectError } from "next/dist/client/components/redirect";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+export async function signUp(
+  credentials: SignUpValues,
+): Promise<{ error: string }> {
+  // we can't catch the errors in the front end
+
+  try {
+    // if the credentials are invalid, the parse method will throw an error that we can catch catch block
+    const { email, username, password } = signUpSchema.parse(credentials);
+    const passwordHash = await hash(password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    });
+    const userId = generateIdFromEntropySize(10);
+    const existingUsername = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    });
+    if (existingUsername) {
+      return {
+        error: "Username is already taken",
+      };
+    }
+    const existingEmail = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+    if (existingEmail) {
+      return {
+        error: "Email is already taken",
+      };
+    }
+    await prisma.user.create({
+      data: {
+        id: userId,
+        username,
+        displayName: username,
+        email,
+        passwordHash,
+      },
+    });
+    const session = await lucia.createSession(userId, {});
+    const sessionCookies = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookies.name,
+      sessionCookies.value,
+      sessionCookies.attributes,
+    );
+    return redirect("/");
+  } catch (error) {
+    // redirect function by default throw an error that caught by the catch block
+    // and it will not work but to make it work we have to check if the error is a redirect error
+    // and if it is then we have to throw the error so that the redirect function can work
+    if (isRedirectError(error)) throw error;
+    console.error(error);
+    return {
+      error: "Something went wrong. Please try again later.",
+    };
+  }
+}
---
+++ src/auth.ts
@@ -0,0 +1,82 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import { Lucia, Session, User } from "lucia";
+import { cookies } from "next/headers";
+import { cache } from "react";
+import prisma from "./lib/prisma";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes(databaseUserAttributes) {
+    return {
+      id: databaseUserAttributes.id,
+      username: databaseUserAttributes.username,
+      displayName: databaseUserAttributes.displayName,
+      avatarUrl: databaseUserAttributes.avatarUrl,
+      googleId: databaseUserAttributes.googleId,
+    };
+  },
+});
+// this is how you extend the databaseUserAttributes type
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+  googleId: string | null;
+}
+// cache here is for example i have multiple component in same page where i will get the session
+// so i will cache the session so that i dont have to call the api again and again for every component
+// but of course if i refresh the page then i have to call the api again to get the session
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+    // here do some comparison with the database session  and the cookies
+
+    const result = await lucia.validateSession(sessionId);
+    console.log(result);
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+
+    return result;
+  },
+);
