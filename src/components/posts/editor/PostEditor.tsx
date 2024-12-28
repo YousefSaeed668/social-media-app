bec152a61b200a7fd9aec77a98c16361c4a418ae
yousefsaeed668@gmail.com <yousefsaeed668@gmail.com>
2024-12-28 22:51:13 +0200

    enhance PostEditor component; integrate drag-and-drop file upload and clipboard paste support

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -14,6 +14,7 @@ import { ImageIcon, Loader2, X } from "lucide-react";
 import useMediaUpload, { Attachment } from "./useMediaUpload";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useDropzone } from "@uploadthing/react";
 
 export default function PostEditor() {
   const { user } = useSession();
@@ -27,6 +28,10 @@ export default function PostEditor() {
     removeAttachment,
     reset: resetMediaUploads,
   } = useMediaUpload();
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: startUpload,
+  });
+  const { onClick, ...rootProps } = getRootProps();
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
@@ -58,14 +63,28 @@ export default function PostEditor() {
     );
     editor?.commands.clearContent();
   }
+  function onPaste(e: React.ClipboardEvent<HTMLInputElement>) {
+    const files = Array.from(e.clipboardData.items)
+      .filter((item: DataTransferItem) => item.kind === "file")
+      .map((item: DataTransferItem) => item.getAsFile()) as File[];
+    startUpload(files);
+  }
+
   return (
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
-        <EditorContent
-          editor={editor}
-          className="max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3"
-        />
+        <div {...rootProps} className="w-full">
+          <EditorContent
+            onPaste={onPaste}
+            editor={editor}
+            className={cn(
+              "max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3",
+              isDragActive && "outline-dashed",
+            )}
+          />
+          <input {...getInputProps()} className="sr-only" />
+        </div>
       </div>
 
       {!!attachments.length && (
